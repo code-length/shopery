06253226b5694aec3e9217ecc30f32f752a33c1a
kenmaqqe <dimetriytalibal@gmail.com>
2025-02-19 22:11:54 +0200

    refactor: 💡 add fields in product models from entities diagram
    ✅ Closes: SHRP-8 Mock backend

--- apps/be-api/src/app/models/product.models.ts
+++ apps/be-api/src/app/models/product.models.ts
@@ -5,6 +5,12 @@ const ProductSchema = new mongoose.Schema(
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String },
+    categoriesId: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    popularTag: { type: Array, required: true },
+    quantityInStock: { type: Number, required: true },
+    brand: { type: String },
+    img: { type: String },
   },
   { timestamps: true }
 );
