73678428b2b531c9745e676fb02f4181ae85d35c
kenmaqqe <dimetriytalibal@gmail.com>
2025-02-23 20:49:51 +0200

    refactor: 💡 corect formationg in db.ts and use port from .env

--- apps/be-api/src/main.ts
+++ apps/be-api/src/main.ts
@@ -1,7 +1,9 @@
 import Fastify from 'fastify';
 import dbConnector from './app/plugins/db'
 import productRoutes from './app/routes/products';
-import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const fastify = Fastify({ logger: true });
 
@@ -14,7 +16,7 @@ fastify.get('/', async () => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: parseFloat(process.env.BE_ADMIN_PORT), host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
