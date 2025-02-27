0f31cfe5739b146b40204fe83a60da0d4340b402
kenmaqqe <dimetriytalibal@gmail.com>
2025-02-27 14:16:02 +0200

    feat: 🎸 close component

--- apps/fe-admin/src/pages/DashboardPage.tsx
+++ apps/fe-admin/src/pages/DashboardPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Close from '@shopery/ui-shared';
 
 const DashboardPage = () => {
-  return <section>Dashboard</section>;
+  return (
+    <section>
+      Dashboard
+      <Close />
+    </section>
+  );
 };
 
 export default DashboardPage;
---
+++ libs/ui/ui-shared/src/lib/components/Close/Close.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Close.module.scss';
+
+const Close = () => {
+  return <button className={styles.Close}>x</button>;
+};
+
+export default Close;
