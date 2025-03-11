5d92107befe461e648d5ab045d9a17fe8b20d0ad
kenmaqqe <dimetriytalibal@gmail.com>
2025-03-11 20:38:37 +0200

    feat: 🎸 create order module

--- apps/be-api/src/main.ts
+++ apps/be-api/src/main.ts
@@ -3,6 +3,7 @@ import Fastify from 'fastify';
 import { healthCheckRoutes } from './app/modules/health-check';
 import { productAdminRoutes } from './app/modules/products';
 import { userAdminRoutes } from './app/modules/users/routes';
+import { orderAdminRoutes } from './app/modules/orders';
 import dbConnector from './app/shared/plugins/db';
 dotenv.config();
 
@@ -11,6 +12,7 @@ const fastify = Fastify({ logger: true });
 fastify.register(dbConnector);
 fastify.register(productAdminRoutes);
 fastify.register(userAdminRoutes);
+fastify.register(orderAdminRoutes);
 fastify.register(healthCheckRoutes);
 
 const start = async () => {
