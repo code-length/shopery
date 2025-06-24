65924c95ad4ad21a17347a89af6378a7ec282abc
kenmaqqe <dimetriytalibal@gmail.com>
2025-06-24 17:53:46 +0300

    fix: 🐛 setting up pagination for Table component

--- libs/ui/ui-shared/src/lib/components/CustomTable/CustomTable.tsx
+++ libs/ui/ui-shared/src/lib/components/CustomTable/CustomTable.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Table } from 'antd';
-import type { TableProps as AntTableProps, ColumnsType } from 'antd/es/table';
 
-export function CustomTable<T>({ ...props }) {
-  return <Table {...props} />;
+export function CustomTable({ ...props }) {
+  return (
+    <Table
+      {...props}
+      pagination={{
+        pageSize: 5,
+      }}
+    />
+  );
 }
 
 export default CustomTable;
